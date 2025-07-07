c713133732baf04202ccc2739fa96524d1b5d65c
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-07-07 17:30:04 +0100

    Fix arithmetic operations and update print statements for clarity

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,12 @@
-number1 = 5
-number2 = 10
+number1 = 10
+number2 = 5
 
 addition = number1 + number2
-subtraction = number2 - number1
+subtraction = number1 - number2
 multiplication = number1 * number2
-division = number2 / number1
+division = number1 / number2
 
 print(f"addition of {number1} and {number2} is {addition}")
-print(f"subtraction of {number1} from {number2} is {subtraction}")
+print(f"subtraction of {number2} from {number1} is {subtraction}")
 print(f"multiplication of {number1} and {number2} is {multiplication}")
-print(f"division of {number2} by {number1} is {division}")
+print(f"division of {number1} by {number2} is {division}")
