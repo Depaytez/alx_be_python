0ce048e6a719f868310fe515693fcd67e554f589
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-07-07 17:26:02 +0100

    Add basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 5
+number2 = 10
+
+addition = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+division = number2 / number1
+
+print(f"addition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} from {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
+print(f"division of {number2} by {number1} is {division}")
